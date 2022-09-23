38202da42d76951ee056e605a371e3d8e282a3fe
Airgamer27 <105240913+Airgamer27@users.noreply.github.com>
2022-09-23 18:31:09 +0200

    Update connect.py
    fixed typo in code

--- connect.py
+++ connect.py
@@ -2,7 +2,7 @@
   # make sure you have installed Python, [Pillow], [Scratchattach], [qrcode]
   # make sure both files are at the same folder!
 import scratchattach as scratch3
-import qrgenerator
+import generator
 
 session = scratch3.login("YOUR USERNAME HERE", "YOUR PASSWORD HERE")
 conn = session.connect_cloud(project_id="PROJECT ID")
@@ -14,7 +14,7 @@ def checkcon():
     return "success"
 @client.request
 def encode(argument1):
-    return qrgenerator.gen_qr(argument1)
+    return generator.gen_qr(argument1)
 
 @client.event
 def on_ready():
