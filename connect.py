49ef8f6b678ef1db0840e960d953a2ee6babc18a
Airgamer27 <105240913+Airgamer27@users.noreply.github.com>
2022-09-23 18:21:30 +0200

    Create connect.py

---
+++ connect.py
@@ -0,0 +1,23 @@
+#     BARCODE GENERATOR
+  # make sure you have installed Python, [Pillow], [Scratchattach], [qrcode]
+  # make sure both files are at the same folder!
+import scratchattach as scratch3
+import qrgenerator
+
+session = scratch3.login("YOUR USERNAME HERE", "YOUR PASSWORD HERE")
+conn = session.connect_cloud(project_id="PROJECT ID")
+
+client = scratch3.CloudRequests(conn)
+
+@client.request
+def checkcon(): 
+    return "success"
+@client.request
+def encode(argument1):
+    return qrgenerator.gen_qr(argument1)
+
+@client.event
+def on_ready():
+    print("Request handler is running")
+
+client.run()
